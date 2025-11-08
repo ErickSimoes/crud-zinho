ab5d9304a2bec4da400bc6919a2cac23fc99dc89
Erick Simões <erick.simoes@live.com>
2025-11-08 10:28:04 -0300

    Implementa o armazenamento de dados

--- storage.py
+++ storage.py
@@ -4,3 +4,8 @@ import json
 def load_data(path):
     with open(path, 'r', encoding='utf-8') as file:
         return json.load(file)
+
+
+def save_data(path, data):
+    with open(path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
