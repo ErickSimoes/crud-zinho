a0c5ddfc7938037ca24819074151e6aaac31d433
Erick Simões <erick.simoes@live.com>
2025-11-08 10:29:25 -0300

    Implementa o CREATE do CRUD
    Recebe os dados do estudante e salva no arquivo via storage.py

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import storage
+
+
+def input_grades():
+    grades = input('Insira as notas do estudante, separadas por espaço: ').strip().split()
+    
+    grades = [float(g) for g in grades] # list comprehension
+
+    '''number_list = []
+    for g in grades:
+        number_list.append(float(g))
+    return number_list'''
+
+    return grades
+
+
+def save_student(name, grades, data, path):
+    data[name] = grades
+    storage.save_data(path, data)
+
+
 def main():
     print('== CRUD zinho ==')
+    path = 'data.json'
     
     while True:
         print('Comandos disponíveis: create, read, update, delete, list, avg, exit')
@@ -7,6 +29,12 @@ def main():
 
         if cmd == 'exit':
             break
+
+        if cmd == 'create':
+            students = storage.load_data(path)
+            name = input('Nome do estudante: ').strip()
+            grades = input_grades()
+            save_student(name, grades, students, path)
 
 
 if __name__ == '__main__':
